b6b999a56d5ea0255cc8b5fb691e5cb6b074aedb
Jakub <qba.nowakowski@gmail.com>
2017-01-05 18:34:17 +0100

    fron end react problem

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -4,8 +4,30 @@ module.exports = {
     devtool:'inline-source-map',
     entry:[
         'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
         './client/app'
-    ]
+    ],
+    output:{
+        path: __dirname + 'public/js',
+        filename: 'app.js',
+        publicPath:'http://localhost:8080/js'
+    },
+    plugins:[
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    resolve: {
+        extensions:['', '.js']
+    },
+    module:{
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loaders: ['react-hot', 'babel-loader'],
+                exclude: /node_modules/
+            }
+        ]
+    }
 }
 
 
